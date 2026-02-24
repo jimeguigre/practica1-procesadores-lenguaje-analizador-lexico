d5f805e1ab38780a9536d6247d229909aa7518e5
eugeniacabanelas <100433061@alumnos.uc3m.es>
2026-02-24 16:39:49 +0100

    arreglo main final

--- main.py
+++ main.py
@@ -9,35 +9,38 @@ lexer = lex.lex(module=scanner)
 
 if len(sys.argv) > 1:
     input_file = sys.argv[1]
-   
-    # creacción del archivo de salida
-    base_name, _ = os.path.splitext(input_file)
-    output_file = base_name + ".token" # .token al final del archivo de salida
+else:
+    # Pon aquí el archivo que quieras probar cuando le des al Play
+    input_file = "tests/test_numeros.lava"
 
-    with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
-        data = f_in.read()
-        lexer.input(data)
+# creacción del archivo de salida
+base_name, _ = os.path.splitext(input_file)
+output_file = base_name + ".token" # .token al final del archivo de salida
+
+with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+    data = f_in.read()
+    lexer.input(data)
        
-        for tok in lexer:
-            # cálculo de columnas llamando a find_column
-            col_start = find_column(data, tok)
+    for tok in lexer:
+        # cálculo de columnas llamando a find_column
+        col_start = find_column(data, tok)
            
-           # cálculo fin de columna
-            if hasattr(tok, 'length'):
-                length = tok.length
-            else:
-                length = len(str(tok.value))
+        # cálculo fin de columna
+        if hasattr(tok, 'length'):
+            length = tok.length
+        else:
+            length = len(str(tok.value))
                 
-            col_end = col_start + length
+        col_end = col_start + length
             
-            #val_str = str(tok.value)
-            #col_end = col_start + len(val_str)
+        #val_str = str(tok.value)
+        #col_end = col_start + len(val_str)
            
-            # formato de salida: { TIPO, VALOR, LÍNEA, COL-INI, COL-FIN }
-            linea_token = f"{{ {tok.type}, {tok.value}, {tok.lineno}, {col_start}, {col_end} }}\n"
+        # formato de salida: { TIPO, VALOR, LÍNEA, COL-INI, COL-FIN }
+        linea_token = f"{{ {tok.type}, {tok.value}, {tok.lineno}, {col_start}, {col_end} }}\n"
            
-            # escribir en el archivo .token
-            f_out.write(linea_token)
+        # escribir en el archivo .token
+        f_out.write(linea_token)
            
-    print(f"Análisis completado. Resultados guardados en: {output_file}")
+print(f"Análisis completado. Resultados guardados en: {output_file}")
 
