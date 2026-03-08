da52dfe1d56b513244ea3e2c05fa8def1e43b9bf
jimeguigre <jimeguigre@gmail.com>
2026-03-08 19:05:51 +0100

    correciones p1

--- lexer.py
+++ lexer.py
@@ -17,7 +17,7 @@ reserved = {
     'print':'PRINT',
     'new':'NEW',
     'record': 'RECORD', 
-    'brak': 'BREAK',
+    'break': 'BREAK',
 }
 
 # definición de tokens
@@ -53,90 +53,164 @@ tokens = [
 ] + list(reserved.values())
 
 # declaración de formatos
-t_EQUALS = r'=='
-
-t_PUNTO_COMA = r';'
-
-t_L_BRACKET = r'\{'
-
-t_R_BRACKET = r'\}'
-
-t_L_PAREN =r'\('
-
-t_R_PAREN = r'\)'
-
-t_MAYOR_IGUAL = r'>='
-
-t_MENOR_IGUAL = r'<='
-
-t_MAYOR = r'>'
-
-t_MENOR = r'<'
-
-t_ASIGNACION = r'='
-
-t_MUL = r'\*'
-
-t_RESTA = r'-'
-
-t_SUMA = r'\+'
-
-t_NOT = r'!'
-
-t_AND = r'&&'
-
-t_OR = r'\|\|'
-
-t_DIV = r'/'
-
-t_PUNTO = r'\.'
-
-t_COMA = r','
-
 def t_COMMENT(t):
     r'//.*|/\*(.|\n)*?\*/'
     t.lexer.lineno += t.value.count('\n')
     pass
 
-def t_CHAR_VALUE(token): #solo 1 caracter (no strings)
-    r'\'.\''
+def t_PUNTO_COMA(token):
+    r';'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_L_BRACKET(token):
+    r'\{'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_R_BRACKET(token):
+    r'\}'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_L_PAREN(token):
+    r'\('
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_R_PAREN(token): 
+    r'\)'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_MAYOR_IGUAL(token):
+    r'>='
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_MENOR_IGUAL(token):
+    r'<='
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_MAYOR(token):
+    r'>'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_MENOR(token):
+    r'<'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_EQUALS(token):
+    r'=='
+    token.column = find_column(token.lexer.lexdata, token)
+    return token
+
+def t_ASIGNACION(token):
+    r'='
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_MUL(token):
+    r'\*'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_RESTA(token):
+    r'-'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_SUMA(token):
+    r'\+'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_NOT(token):
+    r'!'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_AND(token):
+    r'&&'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_OR(token):
+    r'\|\|'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_DIV(token):
+    r'/'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_PUNTO(token):
+    r'\.'
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_COMA(token):
+    r','
+    token.column = find_column(token.lexer.lexdata, token)
+    return token 
+
+def t_CHAR_VALUE(token): 
+    r'\'([^\'\\\n]|\\.)\''
+    token.column = find_column(token.lexer.lexdata, token)
     return token
 
 def t_HEX_VALUE(token):
     r'0x[0-9A-F]+'
+    token.type = 'INT_VALUE' # modificación del tipo a entero
     token.length = len(token.value)
     # conversor a decimal
     token.value = int(token.value, 16)
+    token.column = find_column(token.lexer.lexdata, token)
     return token
 
 def t_BIN_VALUE(token):
-    r'0b[01]+'
+    r'0b[01]+' 
+    token.type = 'INT_VALUE' # modificación del tipo a entero
     token.length = len(token.value)
     # conversor a decimal
     token.value = int(token.value, 2)
+    token.column = find_column(token.lexer.lexdata, token)
     return token
 
 def t_OCT_VALUE(token):
     r'0[0-7]+'
+    token.type = 'INT_VALUE' # modificación del tipo a entero
     token.length = len(token.value)
     # conversor a decimal
     token.value = int(token.value, 8)
+    token.column = find_column(token.lexer.lexdata, token)
     return token   
 
 def t_FLOAT_VALUE(token):
    r'((0|[1-9][0-9]*)\.[0-9]+([eE][-+]?[0-9]+)?|(0|[1-9][0-9]*)[eE][-+]?[0-9]+)'
    token.length = len(token.value)
    token.value = float(token.value)
+   token.column = find_column(token.lexer.lexdata, token)
    return token
 
 def t_INT_VALUE(token):
     r'[1-9][0-9]*|0'
     token.value = int(token.value)
+    token.column = find_column(token.lexer.lexdata, token)
     return token
 
 def t_ID(token):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     token.type = reserved.get(token.value, 'ID')
+    if token.type == 'TRUE':
+        token.value = True  # conversor de True a booleano 
+    elif token.type == 'FALSE':
+        token.value = False # conversor de False a booleano 
+    token.column = find_column(token.lexer.lexdata, token)
     return token
 
 # caracter de nueva línea
@@ -149,7 +223,8 @@ t_ignore = ' \t'
 
 #caracteres no reconocidos
 def t_error(token): 
-    print(f'illegal character at {token.lineno}, {token.lexpos}')
+    col = find_column(token.lexer.lexdata, token)
+    print(f'illegal character at line {token.lineno}, column {col}')
     token.lexer.skip(1) # recuperarse del error (se salta el caracter del error)
 
 # contador de columnas 
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import ply.lex as lex
 import sys
 import os
-from lexer import find_column
-
 import lexer as scanner
 lexer = lex.lex(module=scanner)
 
@@ -13,26 +11,26 @@ if len(sys.argv) > 1:
     base_name, _ = os.path.splitext(input_file)
     output_file = base_name + ".token" # .token al final del archivo de salida
 
-    with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
-        data = f_in.read()
-        lexer.input(data)
-       
-        for tok in lexer:
-            # cálculo de columnas llamando a find_column
-            col_start = find_column(data, tok)
-           
-           # cálculo fin de columna
-            if hasattr(tok, 'length'):
-                length = tok.length
-            else:
-                length = len(str(tok.value))
-                
-            col_end = col_start + length
+    try: 
+        with open(input_file, 'r', encoding="utf-8") as f_in, open(output_file, 'w', encoding="utf-8") as f_out:
+            data = f_in.read()
+            lexer.input(data)
+        
+            for tok in lexer:
+                # cálculo de columnas ya calculado en el lexer 
+                col_start = tok.column 
+            
+                # cálculo de la longitud real 
+                length = getattr(tok, 'length', tok.lexer.lexpos - tok.lexpos)
+                col_end = col_start + length
 
-            # formato de salida: { TIPO, VALOR, LÍNEA, COL-INI, COL-FIN }
-            linea_token = f"{{ {tok.type}, {tok.value}, {tok.lineno}, {col_start}, {col_end} }}\n"
-           
-            # escribir en el archivo .token
-            f_out.write(linea_token)
-           
-    print(f"Análisis completado. Resultados guardados en: {output_file}")
+                # formato de salida: { TIPO, VALOR, LÍNEA, COL-INI, COL-FIN }
+                linea_token = f"{{ {tok.type}, {tok.value}, {tok.lineno}, {col_start}, {col_end} }}\n"
+            
+                # escribir en el archivo .token
+                f_out.write(linea_token)
+            
+        print(f"Análisis completado. Resultados guardados en: {output_file}")
+
+    except FileNotFoundError:
+        print(f"Error: El archivo '{input_file}' no existe.")
